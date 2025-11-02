a0c49f963806695665b94fff5a9eb47226e20455
farihanjum <farihaanjum.24csedu.022@gmail.com>
2025-11-02 13:18:28 +0600

    aa

--- components/header.js
+++ components/header.js
@@ -7,40 +7,43 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header>
-        <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-        <h1><a class="navbar-brand" href="/"> Fariha Anjum</a></h1>
-        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="navbarCollapse" style="">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/education.html">Education</a>
-            </li>
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/experience.html">Experience</a>
-            </li>
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/skills.html">Skills</a>
-            </li>
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/projects.html">Projects</a>
-            </li> 
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/publications.html">Publications</a>
-            </li>
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/blogposts.html">Blogs</a>
-            </li>
-            <li class="nav-item mr-2"> 
-              <a class="nav-link" href="/pages/photogallery.html">Photo Gallery</a>
-            </li>
-          </ul>
-        </div>
-        </nav>
+      <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+      <!-- <h1><a class="navbar-brand" href="/"> Fariha Anjum</a></h1> -->
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="navbarCollapse" style="">
+      <ul class="navbar-nav ml-auto">
+      <li class="nav-item mr-2">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item mr-2">
+        <a class="nav-link" href="/pages/education.html">Education</a>
+      </li>
+      <li class="nav-item mr-2">
+        <a class="nav-link" href="/pages/experience.html">Experience</a>
+      </li>
+      <li class="nav-item mr-2">
+        <a class="nav-link" href="/pages/projects.html">Projects</a>
+      </li> 
+      <li class="nav-item mr-2">
+        <a class="nav-link" href="/pages/publications.html">Publications</a>
+      </li>
+      <li class="nav-item mr-2">
+        <a class="nav-link" href="/pages/blogposts.html">Blogs</a>
+      </li>
+      <li class="nav-item mr-2"> 
+        <a class="nav-link" href="/pages/photogallery.html">Gallery</a>
+      </li>
+      <li class="nav-item mr-2">
+        <a class="nav-link" href="/pages/cv.html">CV</a>
+      </li>
+      <li class="nav-item mr-2">
+        <a class="nav-link" href="/pages/bookshelf.html">Bookshelf</a>
+      </li>
+      </ul>
+      </div>
+      </nav>
       </header>
     `;
   }
